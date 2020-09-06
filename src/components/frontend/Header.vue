f4e2fb8ba0704cab5bd534534c6a57bb587be25d
Aiden Kao <tomkao761116@gmail.com>
2020-09-06 13:02:29 +0800

    add fuction : click logo can go back to index

--- src/components/frontend/Header.vue
+++ src/components/frontend/Header.vue
@@ -6,7 +6,7 @@
           <div class="col-4">
           </div>
           <div class="col-4 d-flex justify-content-center align-items-center">
-            <a class="blog-header-logo" href="#"><img class="img-responsive" src="../../assets/imgs/logo.png" alt="Aiden's Garden"></a>
+            <router-link to="/" class="blog-header-logo"><img class="img-responsive" src="../../assets/imgs/logo.png" alt="Aiden's Garden"></router-link>
           </div>
           <div class="col-4 d-flex justify-content-end align-items-center">
             <span data-toggle="tooltip" title="查看購物車"><router-link to="/cart" class="text-sensaicha p-2 ml-2 mr-2"><i
