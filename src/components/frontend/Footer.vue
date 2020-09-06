37415a014c8f53f7c58ecbc7f5340b8d1dfc4db1
Aiden Kao <tomkao761116@gmail.com>
2020-09-06 13:49:51 +0800

    adjust the footer

--- src/components/frontend/Footer.vue
+++ src/components/frontend/Footer.vue
@@ -1,8 +1,7 @@
 <template>
   <footer class="my-footer">
-    <p class="text-sensaicha">本網站僅供教學練習使用，並無商業用途</p>
-    <p class="text-sensaicha">Copyright © 2020 Aiden's Garden. Some Rights Reserved.</p>
-    <p class="text-sensaicha">All photos are from <a href="https://unsplash.com/">unsplash.com</a></p>
+    <p>本網站僅供教學練習使用，並無商業用途</p>
+    <p>Copyright © 2020 Aiden's Garden. Some Rights Reserved.</p>
   </footer>
 </template>
 
